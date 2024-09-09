507d7272376b559e2f32f676bc109a53ac24062c
betül aktoprak <betulaktoprak112@gmail.com>
2024-09-09 21:46:56 +0300

    lesson4

--- ERP.Server.WebAPI/Program.cs
+++ ERP.Server.WebAPI/Program.cs
@@ -3,6 +3,8 @@ using ERP.Server.Infrastructure;
 using ERP.Server.WebAPI.BackgroundServices;
 using ERP.Server.WebAPI.Middlewares;
 using ERP.Server.WebAPI.Utilities;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors();
@@ -21,6 +23,17 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddHostedService<OutboxBackgroundService>();
 
 builder.Services.AddServiceTool();
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console(restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information)
+    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat = "mylogs",
+        MinimumLogEventLevel = Serilog.Events.LogEventLevel.Information
+    })
+    .CreateLogger();
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
